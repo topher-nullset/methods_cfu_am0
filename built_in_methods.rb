569a1ce9db7a52979c00ce75c076508c88c09dee
Chris Cullinane <chriscullinane@protonmail.com>
2023-04-02 13:38:32 -0800

    Add comments and example answers

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,29 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# The include? method is called on the string object "Hello World"
+# One string argument is passed asking whether the string object includes the string argument
+# The return is true because "Hello" is inside "Hello World"
 "Hello World".include?("Hello")
 
-
+# The end_with? method is called on the sting object "Hello World"
+# One string argument is passed asking whether the string object ends with the string argument
+# The return is false because "Hello" is not at the end of "Hello World" (o and d mismatch, even if l and l match)
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method is called on the sting object "Hello World"
+# One string argument is passed asking whether the string object ends with the string argument
+# The return is true because "rld" == "rld"
 "Hello World".end_with?("rld")
 
-
+# Method even? is called on int obj 
+# No arguments, method asks only if the int obj is even
+# Return is true because twelve is even
 12.even?
 
-
+# Method next is called on int obj
+# No arguments, ints are 1D and have a simple ordering
+# Nhe return is 19 because 19 == 18 +=1
 18.next
 
 
@@ -47,10 +57,24 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
-
+number1 = 42
+number2 = 24
+# lcm asks for the least common multiple of two integers, the first integer is acted on and the second is the argument
+number1.lcm(number2)
+# gcd asks for the greats common divisor of two integers, the structure is the same as lcm
+number1.gcd(number2)
+# gcdlcm runs both gcd and lcm then returns both answers inside of an array
+number1.gcdlcm(number2)
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+me_now = ["Chris", 28]
+me_then = ["Chris Culliane", 16]
+# This selects the 1st element in the array and because the types match, adds the arg string to the end of the array string
+me_now[0].concat(" Cullinane")
+# Selects the 2nd elements in the array and increments it by one for a birthday or w/e (this is not permanent)
+me_now[1].next
