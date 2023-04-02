7a4b24c22f34110007359298610aa8294b726b3a
Chris Cullinane <chriscullinane@protonmail.com>
2023-04-02 14:56:38 -0800

    Add custom methods for examples

--- final_practice.rb
+++ final_practice.rb
@@ -4,21 +4,33 @@
 # Use the puts or print command to see the return value in the console.
 
 
-# 1: Write a method named greeting that returns a string with a general greeting. 
+# 1: Write a method named greeting that returns a string with a general greeting.
+def greeting
+    hello = "Hello and welcome, please enjoy yourself!"
+end
+p greeting
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+# What is the return value of your method?              The String variable
+# How many arguments did you pass your method?          No arguments needed
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
+def custom_greeting(name)
+    hiya = "Hi there #{name}"
+end
+p custom_greeting("Chris")
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method?                  
+# How many arguments did you pass your method?              One argument used to pass in a name
+# What data type was your argument(s)?                      string
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-
+def namer(first, middle, last)
+    full = first + " " + middle + " " + last
+    return "Welcome to the party #{full}"
+end
+p namer("Chris", "Malloy", "Cullinane")
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
@@ -26,23 +38,37 @@
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
+def square(int)
+    sqrd = int ** 2
+    return "#{int} squared is #{sqrd}"
+end
+p square(7)
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method?                      string 
+# How many arguments did you pass your method?                  one    
+# What data type was your argument(s)?                          int
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+def check_stock(stock, item)
+    if stock >= 4
+        "#{item} is Stocked!"
+    elsif stock > 0 && stock < 4
+        "#{item} is Running Low!@!"
+    elsif stock == 0
+        "#{item} is Out Of Stock!@!@!"
+    end
+end    
 
-check_stock(4, "Coffee");
+p check_stock(4, "Coffee");
 # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
+p check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
+p check_stock(0, "Cheese");
 # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
+p check_stock(1, "Salsa");
 # => "Salsa - running LOW"
